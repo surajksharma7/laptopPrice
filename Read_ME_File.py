4b25f793976ca7ac593b036e2854c8587cc91f9d
suraj kumar <140508798+surajksharma7@users.noreply.github.com>
2024-02-08 13:49:34 +0530

    Add files via upload

---
+++ Read_ME_File.py
@@ -0,0 +1,13 @@
+""" README page for Real Estate Project. """
+
+from pathlib import Path
+import streamlit as st
+
+
+README_PATH = Path("README.md")
+st.set_page_config("README.md", "📝", "wide")
+
+try:
+    st.markdown(README_PATH.read_text())
+except FileNotFoundError:
+    st.error("README.md file not found.", icon="🔥")
